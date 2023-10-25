11a97409e3b475dfd95e51c4bfe782fdb82f490c
Gaurav Pekhale <pekhaleg1122@gmail.com>
2023-10-25 23:12:36 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,6 @@
+console.log("Jay Ganesha");                
+
+
+// node Js  - Envirment For Running Javascript 
+// Dino Js - Envirment For Running Javascript 
+// v8 Engin  - Envirment For Running Javascript 
