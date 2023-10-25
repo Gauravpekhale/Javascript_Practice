9c2f97736e3a56ee1850c353714d2d2c81901af2
Gaurav Pekhale <89195306+Gauravpekhale@users.noreply.github.com>
2023-10-25 18:25:21 +0000

    DataTypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId = 10009;
+let email = "p@gmail.vom"
+var name = "GAurav";           // var is Deprecated ... so we avoid to use it 
+cityName = "Nashik";
+
+let state ;
+
+//accountId = 23344 ;  // error Can not br Reassigned Beacause Its Const 
+console.log(accountId)     
+console.log(name);
+
+/*
+Prefere Not to use 'Var' ... Beacuse Var gives isses in Block Scope and Functional Scope 
+*/
+console.table([email,name,email])
+
+
+console.log(state);     // Undefined
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"            // This is helps to tell node-env that i am using new version of javascript
+
+ // alert(3+3)         -> We are using Node js and Not a Browser Console ... alert has used in difrent way 
+
+
+ // Data Types 
+ // number  -> Range 2^53 
+ let iNO = 10;
+ console.log(typeof(iNO))    // o/p -> number
+ // bigInt -> to store Big number data 
+
+
+ // string 
+ let name = ""
+ console.log(typeof(name))       // o/p -> string
+ // null  -> its standlone value in Javascipt 
+ let x =null
+ console.log(typeof(null))        // o/p -> object
+ // Undefined 
+ let accountId;
+ console.log(typeof(accountId))    // o/p -> undefined
+ // Symbol --. To find uniquness the compnents ex. in Angular or in React 
+
+
+ // object -> Instance of class
+
